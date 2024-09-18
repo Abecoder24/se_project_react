670def75f6303eb27a6fb9ac12639de23cbb1738
shubhamskm <programminghead7@gmail.com>
2024-09-18 07:25:09 +0530

    toggleSWITCH

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import "./Header.css"
 import logo from "../../assets/Logo.svg"
 import avatar from "../../assets/Avatar.svg"
-import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch"
 import { Link } from "react-router-dom";
 
 function Header({ handleAddClick, weatherData }) {
